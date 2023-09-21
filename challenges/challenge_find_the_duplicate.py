d8477bda2b0e37f2e59764379bbb711d044e9fb4
vieira <vini.clau0@gmail.com>
2023-09-21 11:21:48 -0300

    feat: challenge duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
+def validate_list(nums):
+    if not all(isinstance(num, int) and num >= 0 for num in nums) or len(nums) <= 1:
+        return False
+    return True
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_list(nums):
+        return False
+
+    numbers = set()
+    for num in nums:
+        if num in numbers:
+            return num
+        numbers.add(num)
+
+    return False
