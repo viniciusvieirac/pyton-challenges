2114bb2d4badc6af65af27444257e2660357a4f7
vieira <vini.clau0@gmail.com>
2023-09-21 10:48:52 -0300

    feat: challenge palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
+    if len(word) == 0:
+        return False
+    
     if low_index >= high_index:
         return True
-    if word[low_index] == word[high_index]:
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
+    if word[low_index] == word[high_index]:
+
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    
     return False
     """Faça o código aqui."""
     raise NotImplementedError
