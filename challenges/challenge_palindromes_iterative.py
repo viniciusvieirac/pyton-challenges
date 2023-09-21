e9014bf8d873289aa861246d0e50107311351b41
vieira <vini.clau0@gmail.com>
2023-09-21 11:24:18 -0300

    feat: challenge palindrome iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or not word:
+        return False
+
+    word = word.lower()
+    left = 0
+    right = len(word) - 1
+
+    while left < right:
+        if word[left] != word[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
