e728939424030db9a32cd2ff030345c59d7d1720
vieira <vini.clau0@gmail.com>
2023-09-21 10:27:22 -0300

    feat: challenge study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
 def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
+    
+    for student_entrance, student_exit in permanence_period:
+        if not isinstance(student_entrance, int) or not isinstance(student_exit, int):
+            return None
+
+    student = 0
+
+    for student_entrance, student_exit in permanence_period:
+        if student_entrance <= target_time <= student_exit:
+            student += 1
+
+    return student
+
     """Faça o código aqui."""
     raise NotImplementedError
