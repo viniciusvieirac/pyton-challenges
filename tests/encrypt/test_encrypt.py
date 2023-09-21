24af6026a91cd23f01c81f85992d6128e6b18d06
vieira <vini.clau0@gmail.com>
2023-09-21 10:45:45 -0300

    feat: challenge test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    encrypted = encrypt_message('vasco', 11)
+    assert encrypted == 'ocsav'
+
+    encrypted = encrypt_message('vasco', 2)
+    assert encrypted == 'ocs_av'
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(1998, 1)
+
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('vasco', 'dois')
+
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('vasco', 19.98)
